5e2c905bc949630163fcf9dae13307c14eea59d5
I-Dvorianska <idvoryanska@gmail.com>
2021-09-09 21:46:12 +0200

    added loader spinner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 import Searchbar from "./Searchbar/Searchbar";
 import "./App.css";
 import ImageGallery from "./ImageGallery/ImageGallery";
@@ -10,27 +12,30 @@ class App extends Component {
     searchText: "",
     images: [],
     page: 1,
+    loading: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
     const { page, searchText } = this.state;
 
     if (prevState.searchText !== searchText) {
-      this.setState({ images: [] });
+      this.setState({ images: [], loading: true });
 
       fetchImages(searchText, 1).then((res) => {
         const imagesData = res.hits;
-        this.setState({ images: imagesData });
+        this.setState({ images: imagesData, loading: false });
       });
     }
 
     if (prevState.page !== page) {
       if (page !== 1) {
+        this.setState({ loading: true });
         fetchImages(searchText, page)
           .then((res) => {
             const imagesData = res.hits;
             this.setState({
               images: [...this.state.images, ...imagesData],
+              loading: false,
             });
           })
           .finally(this.scrollToBottom);
@@ -57,12 +62,23 @@ class App extends Component {
 
   render() {
     const { getSearchFieldText, changePageNumber } = this;
-    const { images, page } = this.state;
+    const { images, page, loading } = this.state;
 
     return (
       <>
         <Searchbar onSubmit={getSearchFieldText} />
+
         {images.length > 0 && <ImageGallery images={images} />}
+        {loading && (
+          <Loader
+            type="Circles"
+            color="#46c5f0"
+            height={40}
+            width={60}
+            timeout={3000}
+            style={{ marginLeft: 620, marginTop: 15 }}
+          />
+        )}
         {images.length > 0 && <Button page={page} onLoad={changePageNumber} />}
       </>
     );
